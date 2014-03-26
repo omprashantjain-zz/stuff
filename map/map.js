58a9d4035a54427c6c98695c2162a1d9a59662e7
Prashant Jain <prashant.jain@mubble.in>
2014-03-26 10:51:05 +0530

    MAP: fetching list of nearby places by location and radius.

--- map/map.js
+++ map/map.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
     this.inputBox  = $('#latlng');
     this.button    = $('#get-result');
     this.resultBox = $('#result-area');
+    this.distance = $('#distance');
   }
   
   Map.prototype.codeLatLng = function() {
@@ -14,17 +15,27 @@ $(document).ready(function(){
     var lat = parseFloat(latlngStr[0]);
     var lng = parseFloat(latlngStr[1]);
     var latlng = new google.maps.LatLng(lat, lng);
-    var result = "Your Address is: ";
-    var components;
-    context.geocoder.geocode({'latLng': latlng}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        if (results[0]) {
-          result += results[0].formatted_address + '\n';
-          components = results[0].address_components;
-          for(;i < components.length ;i++) {
-			  result += '\n' + components[i].types[0] + ': ' + components[i].long_name;
-		  }
-		  result += '\n\n' + 'Location Accuracy is: ' + results[0].geometry.location_type;
+    var result = "Places nearby this location are: ";
+    
+    var map = new google.maps.Map(document.getElementById('map-canvas'), {
+      center: latlng,
+      zoom: 15
+    });
+
+    var request = {
+      location: latlng,
+      radius: context.distance.val()
+    };
+  
+    var service = new google.maps.places.PlacesService(map);
+
+    service.nearbySearch(request, function(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        if (results) {
+					console.log(results);
+          for(;i < results.length ;i++) {
+			      result += '\n\n"' + results[i].name + '" and is of type: ' + results[i].types.join(', ');
+		      }
           context.resultBox.val(result);
         } else {
           alert('No results found');
