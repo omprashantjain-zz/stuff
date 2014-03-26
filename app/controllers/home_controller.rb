f018233382dfd9e3184483955c279aec0e388899
Prashant Jain <prashant.jain@mubble.in>
2014-03-26 09:26:57 +0530

    Deleted Rails project

--- app/controllers/home_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomeController < ApplicationController
-  def index
-    @jibbers = Jibber.all
-  end
-end
--- test/unit/helpers/jibber_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class JibberHelperTest < ActionView::TestCase
-end
