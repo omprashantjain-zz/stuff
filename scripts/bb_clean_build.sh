dddd369e6a9fd1adb78fd7093a728a1ad4e9e9e4
Prashant Jain <tensor@amazon.com>
2018-09-20 12:45:46 +0530

    Added sivis

--- scripts/bb_clean_build.sh
+++
@@ -1,2 +0,0 @@
-cd ~/workspaces/SWIPERTOL/src/REGSWIPEFragments
-for pkg in REGSWIPEFragments REGSWIPEFragmentsAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack;do cd ../${pkg} && brazil-build clean && brazil-build; done
