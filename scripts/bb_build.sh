39c182264a0799ff2c2d26aba35825187c36c30f
Prashant Jain <tensor@amazon.com>
2018-10-17 17:30:10 +0530

    updates

--- scripts/bb_build.sh
+++ scripts/bb_build.sh
@@ -1,2 +1,2 @@
 cd ~/workspaces/SWIPERTOL/src/REGSWIPEFragments
-for pkg in REGSWIPEFragments REGSWIPEFragmentsAssets SIVISFragment SIVISFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack;do cd ../${pkg} && echo 'Building ${pkg}' && brazil-build; done
+for pkg in REGSWIPEFragments REGSWIPEFragmentsAssets KycEntityFragment KycEntityFragmentAssets SIVISFragment SIVISFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack;do cd ../${pkg} && echo 'Building ${pkg}' && brazil-build; done
--- scripts/bb_build_assets.sh
+++
@@ -1,2 +0,0 @@
-cd ~/workspaces/SWIPERTOL/src/REGSWIPEFragments
-for pkg in REGSWIPEFragmentsAssets SIVISFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLWebStack;do cd ../${pkg} && echo 'Building ${pkg}' && brazil-build; done
