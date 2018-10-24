3579b2b75bea948777fcfa116f3a6b07bf224517
Prashant Jain <tensor@amazon.com>
2018-10-24 14:32:05 +0530

    update

--- scripts/bb_clean.sh
+++ scripts/bb_clean.sh
@@ -1,2 +1,2 @@
 cd ~/workspaces/SWIPERTOL/src/REGSWIPEFragments
-for pkg in REGSWIPEFragments REGSWIPEFragmentsAssets KycEntityFragment KycEntityFragmentAssets SIVISFragment SIVISFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack;do cd ../${pkg} && brazil-build clean; done
+for pkg in REGSWIPEFragments REGSWIPEFragmentsAssets SWIPEDocumentOCRFragment SWIPEDocumentOCRFragmentAssets KycEntityFragment KycEntityFragmentAssets SIVISFragment SIVISFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack;do cd ../${pkg} && brazil-build clean; done
