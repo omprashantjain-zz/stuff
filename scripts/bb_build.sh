fb954e64e07906e462f1f2602a430c89ef278d30
Prashant Jain <tensor@amazon.com>
2018-11-05 20:52:19 +0530

    update

--- scripts/bb_build.sh
+++ scripts/bb_build.sh
@@ -1,2 +1,3 @@
+brazil workspace --sync --metadata
 cd ~/workspaces/SWIPERTOL/src/REGSWIPEFragments
-for pkg in KycBusinessTypeFragment KycBusinessTypeFragmentAsset REGSWIPEFragments REGSWIPEFragmentsAssets KycEntityFragment KycEntityFragmentAssets SIVISFragment SIVISFragmentAssets SWIPEDocumentOCRFragment SWIPEDocumentOCRFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack;do cd ../${pkg} && echo "Building $pkg" && brazil-build; done
+for pkg in KycBusinessTypeFragment KycBusinessTypeFragmentAsset REGSWIPEFragments REGSWIPEFragmentsAssets SIVISFragment SIVISFragmentAssets SWIPEDocumentOCRFragment SWIPEDocumentOCRFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack;do cd ../${pkg} && echo "Building $pkg" && brazil-build; done
--- scripts/bb_pull.sh
+++ scripts/bb_pull.sh
@@ -1,5 +1,5 @@
 cd ~/workspaces/SWIPERTOL/src/REGSWIPEFragments
-for pkg in  KycBusinessTypeFragment KycBusinessTypeFragmentAsset REGSWIPEFragments REGSWIPEFragmentsAssets SWIPEDocumentOCRFragment SWIPEDocumentOCRFragmentAssets KycEntityFragment KycEntityFragmentAssets SIVISFragment SIVISFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack; do
+for pkg in  KycBusinessTypeFragment KycBusinessTypeFragmentAsset REGSWIPEFragments REGSWIPEFragmentsAssets SWIPEDocumentOCRFragment SWIPEDocumentOCRFragmentAssets SIVISFragment SIVISFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack; do
   cd ../${pkg}
   if git diff-index --quiet HEAD --; then
     git pull;
