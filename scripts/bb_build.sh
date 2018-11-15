3e0e26006c776e77985f3cbab74ad03bbd7c36ae
Prashant Jain <tensor@amazon.com>
2018-11-15 18:47:38 +0530

    update

--- scripts/bb_build.sh
+++ scripts/bb_build.sh
@@ -1,3 +1,3 @@
 brazil workspace --sync --metadata
 cd ~/workspaces/SWIPERTOL/src/REGSWIPEFragments
-for pkg in REGSWIPEFragments REGSWIPEFragmentsAssets SIVISFragment SIVISFragmentAssets SWIPEDocumentOCRFragment SWIPEDocumentOCRFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack;do cd ../${pkg} && echo "Building $pkg" && brazil-build; done
+for pkg in REGSWIPEFragments REGSWIPEFragmentsAssets SIVISFragment SIVISFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack;do cd ../${pkg} && echo "Building $pkg" && brazil-build; done
--- scripts/bb_clean.sh
+++ scripts/bb_clean.sh
@@ -1,2 +1,2 @@
 cd ~/workspaces/SWIPERTOL/src/REGSWIPEFragments
-for pkg in REGSWIPEFragments REGSWIPEFragmentsAssets SWIPEDocumentOCRFragment SWIPEDocumentOCRFragmentAssets SIVISFragment SIVISFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack;do cd ../${pkg} && brazil-build clean; done
+for pkg in REGSWIPEFragments REGSWIPEFragmentsAssets SIVISFragment SIVISFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack;do cd ../${pkg} && brazil-build clean; done
