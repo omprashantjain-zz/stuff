fe7d2eb275284c1632f0622233f4225c63e2b303
Prashant Jain <tensor@amazon.com>
2019-03-13 16:21:24 +0530

    update

--- scripts/bb_pull.sh
+++ scripts/bb_pull.sh
@@ -1,3 +1,4 @@
+brazil ws sync
 cd ~/workspaces/SWIPERTOL/src/REGSWIPEFragments
 for pkg in REGSWIPEFragments REGSWIPEFragmentsAssets SIVISFragment SIVISFragmentAssets SWIPERTOLLayouts SWIPERTOLWebStackAssets SWIPERTOLEndPointConfig SWIPERTOLWebStack; do
   cd ../${pkg}
